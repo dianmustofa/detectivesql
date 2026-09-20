6ec941beb10cc0138f0625689c88ffe72eb8b817
dianmustofa <mustofa.d@outlook.com>
2026-09-20 22:42:41 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Memastikan ESLint tidak membatalkan proses build Vercel
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Mencegah kesalahan build akibat tipe variabel tersembunyi
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
