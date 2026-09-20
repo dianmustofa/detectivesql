b5b2b4a504f02e83937c8b567071d89651e1c66b
dianmustofa <mustofa.d@outlook.com>
2026-09-20 22:44:57 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Abaikan error ESLint saat build di Vercel
   eslint: {
-    // Memastikan ESLint tidak membatalkan proses build Vercel
     ignoreDuringBuilds: true,
   },
+  // Abaikan error TypeScript saat build di Vercel
   typescript: {
-    // Mencegah kesalahan build akibat tipe variabel tersembunyi
     ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
-    }
-
-    return config;
   },
 };
 
